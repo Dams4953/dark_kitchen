61a268bc8a195f09ed07dda601f6420692eae801
thomas <scozzari.thomas@yahoo.fr>
2023-12-19 19:23:42 +0100

    thomas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 const collection = [
-<<<<<<< HEAD
   //1
   {
     nom: "Cheez Burger",
@@ -11,59 +10,33 @@ const collection = [
       "4/5",
     ],
     commander: "#",
-  //1
-  {
-    nom: "Cheez Burger",
-    prix: "Prix : 4,50€",
-    picture: "assets/images/burger1.jpeg",
-    genre: ["Burger", "Vegetarien"],
-    ingredients: ["Neat patty, cheez, ketchup, mustard, pickles, onion"],
-    note: [
-      "4/5",
-    ],
-    commander: "#",
-=======
-    //1
-    {
-      nom: "Cheez Burger",
-      prix: "Prix : 4,50€",
-      picture: "assets/images/burger1.jpeg",
-      genre: ["Burger", "Vegetarien"],
-      ingredients:["Neat patty, cheez, ketchup, mustard, pickles, onion"],
-      note: [
-        "4/5",
-      ],
-      commander: "#",
->>>>>>> old-state
-
-},
+  },
 //2
 {
-    nom: "Smash Daddy",
-    prix: "Prix : 6,99€",
-    picture: "assets/images/burger2.jpeg",
-    genre: ["Burger", "Vegan"],
-    ingredients:["double neat smashed patty, double cheez, grilled onion, mustard, stack sauce"],
-    note: [
-      "3/5",
-    ],
-    commander: "#",
-  },
-  //3
-  {
-    nom: "NEW! BBQ Brisket Burger",
-    prix: "Prix : 8,99€",
-    picture: "assets/images/burger3.jpeg",
-    genre: ["Burger", "Vegetarien"],
-    ingredients:["BBQ pulled mushroom 'brisket', double Neat smash patty, cheez, pickles, jalapenos, lettuce, pickled onion, BBQ sauce, herby aioli, mayo"],
-    note: [
-      "3,5/5",
-    ],
-    commander: "#",
-  },
+  nom: "Smash Daddy",
+  prix: "Prix : 6,99€",
+  picture: "assets/images/burger2.jpeg",
+  genre: ["Burger", "Vegan"],
+  ingredients:["double neat smashed patty, double cheez, grilled onion, mustard, stack sauce"],
+  note: [
+    "3/5",
+  ],
+  commander: "#",
+},
+//3
+{
+  nom: "NEW! BBQ Brisket Burger",
+  prix: "Prix : 8,99€",
+  picture: "assets/images/burger3.jpeg",
+  genre: ["Burger", "Vegetarien"],
+  ingredients:["BBQ pulled mushroom 'brisket', double Neat smash patty, cheez, pickles, jalapenos, lettuce, pickled onion, BBQ sauce, herby aioli, mayo"],
+  note: [
+    "3,5/5",
+  ],
+  commander: "#",
+},
 //4
 {
-<<<<<<< HEAD
   nom: "Skinny Fries",
   prix: "Prix : 3,99€",
   picture: "assets/images/frites1.jpeg",
@@ -133,216 +106,11 @@ const collection = [
     "4/5",
   ],
   commander: "#",
-},
-  },
-  //2
-  {
-    nom: "Smash Daddy",
-    prix: "Prix : 6,99€",
-    picture: "assets/images/burger2.jpeg",
-    genre: ["Burger", "Vegan"],
-    ingredients: ["double neat smashed patty, double cheez, grilled onion, mustard, stack sauce"],
-    note: [
-      "3/5",
-    ],
-    commander: "#",
-  },
-  //3
-  {
-    nom: "NEW! BBQ Brisket Burger",
-    prix: "Prix : 8,99€",
-    picture: "assets/images/burger3.jpeg",
-    genre: ["Burger", "Vegetarien"],
-    ingredients: ["BBQ pulled mushroom 'brisket', double Neat smash patty, cheez, pickles, jalapenos, lettuce, pickled onion, BBQ sauce, herby aioli, mayo"],
-    note: [
-      "3,5/5",
-    ],
-    commander: "#",
-  },
-  //4
-  {
-=======
->>>>>>> old-state
-    nom: "Skinny Fries",
-    prix: "Prix : 3,99€",
-    picture: "assets/images/frites1.jpeg",
-    genre: ["Sides"],
-<<<<<<< HEAD
-    ingredients: ["skinny salty skin-on fries"],
-=======
-    ingredients:["skinny salty skin-on fries"],
->>>>>>> old-state
-    note: [
-      "4/5",
-    ],
-    commander: "#",
-  },
-  //5
-  {
-    nom: "Crispy Chick'n Caesar Wrap",
-    prix: "Prix : 6,99€",
-    picture: "assets/images/wrap1.jpeg",
-    genre: ["Wraps", "Vegetarien"],
-<<<<<<< HEAD
-    ingredients: ["fried chick'n, croutons, parmesan, romaine lettuce, caesar dressing, smashed avocado, fresh tortilla wrap"],
-=======
-    ingredients:["fried chick'n, croutons, parmesan, romaine lettuce, caesar dressing, smashed avocado, fresh tortilla wrap"],
->>>>>>> old-state
-    note: [
-      "4,5/5",
-    ],
-    commander: "#",
-  },
-  //6
-  {
-    nom: "Falafel Caesar Wrap",
-    prix: "Prix : 6,99€",
-    picture: "assets/images/wrap2.jpeg",
-    genre: ["Wraps", "Vegetarien"],
-<<<<<<< HEAD
-    ingredients: ["falafel, croutons, parmesan, romaine lettuce, caesar dressing, smashed avocado, fresh tortilla wrap"],
-=======
-    ingredients:["falafel, croutons, parmesan, romaine lettuce, caesar dressing, smashed avocado, fresh tortilla wrap"],
->>>>>>> old-state
-    note: [
-      "4/5",
-    ],
-    commander: "#",
-  },
-  //7
-  {
-    nom: "Cripsy Chick'n Caesar Salad",
-    prix: "Prix : 12,99€",
-    picture: "assets/images/salade1.jpeg",
-    genre: ["Salad", "Vegetarien"],
-<<<<<<< HEAD
-    ingredients: ["crispy chick'n, croutons, parmesan, romaine lettuce, caesar dressing"],
-=======
-    ingredients:["crispy chick'n, croutons, parmesan, romaine lettuce, caesar dressing"],
->>>>>>> old-state
-    note: [
-      "2/5",
-    ],
-    commander: "#",
-  },
-  //8
-  {
-    nom: "Organic Orangeade",
-    prix: "Prix : 3,99€",
-    picture: "assets/images/boisson1.jpeg",
-    genre: ["Drinks"],
-<<<<<<< HEAD
-    ingredients: ["Organic orangeade, this drink contains all the vitamins you need!"],
-=======
-    ingredients:["Organic orangeade, this drink contains all the vitamins you need!"],
->>>>>>> old-state
-    note: [
-      "4/5",
-    ],
-    commander: "#",
-  },
-  //9
-  {
-    nom: "Lemony Lemonade",
-    prix: "Prix : 3,99",
-    picture: "assets/images/boisson2.jpeg",
-    genre: ["Drinks"],
-<<<<<<< HEAD
-    ingredients: ["Organic lemonade, this drink contains all the vitamins you need!"],
-=======
-    ingredients:["Organic lemonade, this drink contains all the vitamins you need!"],
->>>>>>> old-state
-    note: [
-      "4/5",
-    ],
-    commander: "#",
-  },
-];
+}];
 
 let section = document.getElementById('card-container');
 
 for (let elem of collection) {
-    let div = document.createElement('div');
-    div.className = 'card';
-
-  
-    let divimg = document.createElement('div');
-    divimg.className = 'card__image';
-  
-    let img = document.createElement('img');
-    img.src = elem.picture;
-    img.className = 'desktop-image';
-  
-    divimg.appendChild(img);
-  
-    let h2 = document.createElement('h2');
-    h2.className = 'card__genre';
-    h2.textContent = elem.genre.join(', ');
-  
-    let h3 = document.createElement('h3');
-    h3.className = 'card__nom';
-    h3.textContent = elem.nom;
-  
-    let h4 = document.createElement('h4');
-    h4.className = 'card__prix';
-    h4.textContent = elem.prix;
-  
-    let p = document.createElement('p');
-    p.className = 'card__ingredients';
-    p.textContent = elem.ingredients;
-  
-    let divnote = document.createElement('div');
-    divnote.className = 'card__note';
-  
-    let divnoteFirst = document.createElement('div');
-    divnoteFirst.className = 'card__note__first';
-    divnoteFirst.textContent = `Note des clients ${elem.note[0]}`;
-  
-    divnote.appendChild(divnoteFirst);
-  
-    let divlien = document.createElement('div');
-    divlien.className = 'card__lien';
-  
-    let button = document.createElement('a');
-    button.className = 'card__order'
-
-<<<<<<< HEAD
-  let img = document.createElement('img');
-  img.src = elem.picture;
-  img.className = 'desktop-image';
-
-  divimg.appendChild(img);
-
-  let h2 = document.createElement('h2');
-  h2.className = 'card__genre';
-  h2.textContent = elem.genre.join(', ');
-
-  let h3 = document.createElement('h3');
-  h3.className = 'card__nom';
-  h3.textContent = elem.nom;
-
-  let h4 = document.createElement('h4');
-  h4.className = 'card__prix';
-  h4.textContent = elem.prix;
-
-  let p = document.createElement('p');
-  p.className = 'card__ingredients';
-  p.textContent = elem.ingredients;
-
-  let divnote = document.createElement('div');
-  divnote.className = 'card__note';
-
-  let divnoteFirst = document.createElement('div');
-  divnoteFirst.className = 'card__note__first';
-  divnoteFirst.textContent = `Note des clients ${elem.note[0]}`;
-
-  divnote.appendChild(divnoteFirst);
-
-  let divlien = document.createElement('div');
-  divlien.className = 'card__lien';
-
-  let button = document.createElement('a');
-  button.className = 'card__order'
   let div = document.createElement('div');
   div.className = 'card';
 
@@ -384,10 +152,7 @@ for (let elem of collection) {
   divlien.className = 'card__lien';
 
   let button = document.createElement('a');
-  button.className = 'card__order'
-=======
->>>>>>> old-state
-
+  button.className = 'card__order';
   button.href = elem.commander;
   button.textContent = 'Commander le produit';
 
@@ -402,15 +167,8 @@ for (let elem of collection) {
   div.appendChild(divlien);
 
   section.appendChild(div);
-
 }
-<<<<<<< HEAD
 
-/*DarkMode*/
-
-=======
-//darkmode//
->>>>>>> old-state
 const toggleButton = document.getElementById("darkmode-toggle");
 const body = document.body;
 
@@ -419,19 +177,19 @@ toggleButton.addEventListener("click", () => {
 
   let backgroundSection = document.getElementById("section-home");
   let allCards = document.querySelectorAll(".card");
-  let barNav = document.querySelector(".navbar")
-  let categories = document.querySelectorAll(".card__genre")
-  let backgroundFooter = document.querySelector('.footer')
-  let backgroundConfi = document.querySelector('.footer_politiqueConfi')
-  let orders = document.querySelectorAll('.card__order')
-  let ordersName = document.querySelectorAll('.card__nom')
+  let barNav = document.querySelector(".navbar");
+  let categories = document.querySelectorAll(".card__genre");
+  let backgroundFooter = document.querySelector('.footer');
+  let backgroundConfi = document.querySelector('.footer_politiqueConfi');
+  let orders = document.querySelectorAll('.card__order');
+  let ordersName = document.querySelectorAll('.card__nom');
 
   if (body.classList.contains("dark-mode")) {
     backgroundSection.style.backgroundImage = "url('assets/images/darkmode-image.png')";
     backgroundSection.style.backgroundSize = "cover";
-    barNav.style.backgroundColor = "rgb(66, 63, 62)"
-    backgroundFooter.style.backgroundColor = "rgb(66, 63, 62)"
-    backgroundConfi.style.backgroundColor = "rgb(85, 84, 83)"
+    barNav.style.backgroundColor = "rgb(66, 63, 62)";
+    backgroundFooter.style.backgroundColor = "rgb(66, 63, 62)";
+    backgroundConfi.style.backgroundColor = "rgb(85, 84, 83)";
 
     allCards.forEach(card => {
       card.style.backgroundColor = "grey";
@@ -442,21 +200,18 @@ toggleButton.addEventListener("click", () => {
     });
 
     orders.forEach(order => {
-      order.style.backgroundColor = "rgb(85, 84, 83)"
-    })
+      order.style.backgroundColor = "rgb(85, 84, 83)";
+    });
 
     ordersName.forEach(orderName => {
-      orderName.style.color = "rgb(85, 84, 83)"
-    })
-
+      orderName.style.color = "rgb(85, 84, 83)";
+    });
   } else {
     backgroundSection.style.backgroundImage = "url('assets/images/BandeEntete.png')";
-<<<<<<< HEAD
     backgroundFooter.style.backgroundColor = "";
     barNav.style.backgroundColor = "";
     backgroundFooter.style.backgroundColor = "";
     backgroundConfi.style.backgroundColor = "";
-
 
     allCards.forEach(card => {
       card.style.backgroundColor = "";
@@ -468,16 +223,13 @@ toggleButton.addEventListener("click", () => {
 
     orders.forEach(order => {
       order.style.backgroundColor = "";
-    })
-
+    });
 
     ordersName.forEach(orderName => {
-      orderName.style.color = ""
-    })
-
+      orderName.style.color = "";
+    });
   }
-}
-)
+});
 
 
 /*
@@ -507,10 +259,7 @@ element.setAttribute('url', url)
 parent.appendChild(element)
 }
 */
-=======
-    backgroundCard.style.backgroundColor = ""; 
-  }
-});
+
 
 //panier//
 // Créer un tableau avec tous les genres uniques de la collection
@@ -602,4 +351,3 @@ function filterCollection(genre) {
 }
 
 
->>>>>>> old-state
