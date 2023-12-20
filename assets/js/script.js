a2c688306ab8fe6af52eaa729f477a779f009104
Damien <legat@CQJ>
2023-12-20 16:12:51 +0100

    fin modif dam

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,10 +107,257 @@ const collection = [
   ],
   commander: "#",
 }];
+// STOCKER COPIE DE COLLECTION
+const originalCollection = [...collection];
+//AFFICHER LES CARTES
 
 let section = document.getElementById('card-container');
 
+//panier commander produit
+function addPanier(burger){
+  cart.push(burger);
+  console.log(cart);
+}
+
+
 for (let elem of collection) {
+  let div = document.createElement('div');
+  div.className = 'card';
+
+  let divimg = document.createElement('div');
+  divimg.className = 'card__image';
+
+  let img = document.createElement('img');
+  img.src = elem.picture;
+  img.className = 'desktop-image';
+
+  divimg.appendChild(img);
+
+  let h2 = document.createElement('h2');
+  h2.className = 'card__genre';
+  h2.textContent = elem.genre.join(', ');
+
+  let h3 = document.createElement('h3');
+  h3.className = 'card__nom';
+  h3.textContent = elem.nom;
+
+  let h4 = document.createElement('h4');
+  h4.className = 'card__prix';
+  h4.textContent = elem.prix;
+
+  let p = document.createElement('p');
+  p.className = 'card__ingredients';
+  p.textContent = elem.ingredients;
+
+  let divnote = document.createElement('div');
+  divnote.className = 'card__note';
+
+  let divnoteFirst = document.createElement('div');
+  divnoteFirst.className = 'card__note__first';
+  divnoteFirst.textContent = `Note des clients ${elem.note[0]}`;
+
+  divnote.appendChild(divnoteFirst);
+
+  let divlien = document.createElement('div');
+  divlien.className = 'card__lien';
+
+
+//____
+  let button = document.createElement('button');
+    button.className = 'card__order';
+    button.textContent = 'Commander le produit';
+    button.addEventListener("click", ()=>{addPanier(elem)});
+
+
+  divlien.appendChild(button);
+
+  div.appendChild(divimg);
+  div.appendChild(h2);
+  div.appendChild(h3);
+  div.appendChild(h4);
+  div.appendChild(p);
+  div.appendChild(divnote);
+  div.appendChild(divlien);
+
+  section.appendChild(div);
+}
+
+
+const toggleButton = document.getElementById("darkmode-toggle");
+const body = document.body;
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  let backgroundSection = document.getElementById("section-home");
+  let allCards = document.querySelectorAll(".card");
+  let barNav = document.querySelector(".navbar")
+  let categories = document.querySelectorAll(".card__genre")
+  let backgroundFooter = document.querySelector('.footer')
+  let backgroundConfi = document.querySelector('.footer_politiqueConfi')
+  let orders = document.querySelectorAll('.card__order')
+  let ordersName = document.querySelectorAll('.card__nom')
+  let contact = document.querySelector('.footer_contact')
+  let menutitle = document.querySelector('.h2main')
+  let active = document.querySelector('.active')
+
+  if (body.classList.contains("dark-mode")) {
+    backgroundSection.style.backgroundImage = "url('assets/images/darkmode-image.png')";
+    backgroundSection.style.backgroundSize = "cover";
+    barNav.style.backgroundColor = "rgb(66, 63, 62)"
+    backgroundFooter.style.backgroundColor = "rgb(66, 63, 62)"
+    backgroundConfi.style.backgroundColor = "rgb(85, 84, 83)"
+    contact.style.backgroundColor="rgb(66, 63, 62)"
+    menutitle.style.color="rgb(85, 84, 83)"
+    active.style.backgroundColor = "rgb(66, 63, 62)"
+
+    allCards.forEach(card => {
+      card.style.backgroundColor = "grey";
+    });
+
+    categories.forEach(category => {
+      category.style.color = "rgb(85, 84, 83)";
+    });
+
+    orders.forEach(order => {
+      order.style.backgroundColor = "rgb(85, 84, 83)"
+    })
+
+    ordersName.forEach(orderName => {
+      orderName.style.color = "rgb(85, 84, 83)"
+    })
+
+  } else {
+    backgroundSection.style.backgroundImage = "url('assets/images/BandeEntete.png')";
+    backgroundFooter.style.backgroundColor = "";
+    barNav.style.backgroundColor = "";
+    backgroundFooter.style.backgroundColor = "";
+    backgroundConfi.style.backgroundColor = "";
+    contact.style.backgroundColor="";
+    menutitle.style.color="";
+    active.style.backgroundColor ="";
+
+
+    allCards.forEach(card => {
+      card.style.backgroundColor ="";
+    });
+
+    categories.forEach(category => {
+      category.style.color = "";
+    });
+
+    orders.forEach(order => {
+      order.style.backgroundColor = "";
+    })
+
+
+    ordersName.forEach(orderName => {
+      orderName.style.color = ""
+    })
+
+  }
+}
+)
+
+//panier//
+// Créer un tableau avec tous les genres uniques de la collection
+const allGenres = collection.reduce((acc, card) => {
+  card.genre.forEach(genre => {
+      if (!acc.includes(genre)) {
+          acc.push(genre);
+      }
+  });
+  return acc;
+}, []);
+
+const filters = allGenres;  // Utiliser les genres uniques pour les filtres
+const filterButtons = document.getElementById('filter-buttons');
+
+for (const filter of filters) {
+  let button = document.createElement('button');
+  button.textContent = filter;
+  button.addEventListener('click', () => filterCollection(filter));
+  filterButtons.appendChild(button);
+}
+
+//FILTRER CARTES
+
+function filterCollection(genre) {
+// Supprimer les cartes actuelles
+section.innerHTML = '';
+
+// Filtrer la collection en fonction du genre sélectionné
+
+const filteredCards = originalCollection.filter((card) => card.genre.includes(genre));
+
+
+// Afficher les cartes filtrées
+for (const elem of filteredCards) {
+    let div = document.createElement('div');
+    div.className = 'card';
+
+    let divimg = document.createElement('div');
+    divimg.className = 'card__image';
+
+    let img = document.createElement('img');
+    img.src = elem.picture;
+    img.className = 'desktop-image';
+
+    divimg.appendChild(img);
+
+    let h2 = document.createElement('h2');
+    h2.className = 'card__genre';
+    h2.textContent = elem.genre.join(', ');
+
+    let h3 = document.createElement('h3');
+    h3.className = 'card__nom';
+    h3.textContent = elem.nom;
+
+    let h4 = document.createElement('h4');
+    h4.className = 'card__prix';
+    h4.textContent = elem.prix;
+
+    let p = document.createElement('p');
+    p.className = 'card__ingredients';
+    p.textContent = elem.ingredients;
+
+    let divnote = document.createElement('div');
+    divnote.className = 'card__note';
+
+    let divnoteFirst = document.createElement('div');
+    divnoteFirst.className = 'card__note__first';
+    divnoteFirst.textContent = `Note des clients ${elem.note[0]}`;
+
+    divnote.appendChild(divnoteFirst);
+
+    let divlien = document.createElement('div');
+    divlien.className = 'card__lien';
+//____
+    let button = document.createElement('button');
+    button.className = 'card__order';
+    button.textContent = 'Commander le produit';
+    button.addEventListener("click", ()=>{addPanier(elem)});
+
+    divlien.appendChild(button);
+
+    div.appendChild(divimg);
+    div.appendChild(h2);
+    div.appendChild(h3);
+    div.appendChild(h4);
+    div.appendChild(p);
+    div.appendChild(divnote);
+    div.appendChild(divlien);
+
+    section.appendChild(div);
+}
+}
+
+function resetCollection() {
+// Supprimer les cartes actuelles
+section.innerHTML = '';
+
+// Afficher toutes les cartes à nouveau
+for (const elem of originalCollection) {
   let div = document.createElement('div');
   div.className = 'card';
 
@@ -168,193 +415,6 @@ for (let elem of collection) {
 
   section.appendChild(div);
 }
-
-
-const toggleButton = document.getElementById("darkmode-toggle");
-const body = document.body;
-
-toggleButton.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-
-  let backgroundSection = document.getElementById("section-home");
-  let allCards = document.querySelectorAll(".card");
-  let barNav = document.querySelector(".navbar")
-  let categories = document.querySelectorAll(".card__genre")
-  let backgroundFooter = document.querySelector('.footer')
-  let backgroundConfi = document.querySelector('.footer_politiqueConfi')
-  let orders = document.querySelectorAll('.card__order')
-  let ordersName = document.querySelectorAll('.card__nom')
-
-  if (body.classList.contains("dark-mode")) {
-    backgroundSection.style.backgroundImage = "url('assets/images/darkmode-image.png')";
-    backgroundSection.style.backgroundSize = "cover";
-    barNav.style.backgroundColor = "rgb(66, 63, 62)"
-    backgroundFooter.style.backgroundColor = "rgb(66, 63, 62)"
-    backgroundConfi.style.backgroundColor = "rgb(85, 84, 83)"
-
-    allCards.forEach(card => {
-      card.style.backgroundColor = "grey";
-    });
-
-    categories.forEach(category => {
-      category.style.color = "rgb(85, 84, 83)";
-    });
-
-    orders.forEach(order => {
-      order.style.backgroundColor = "rgb(85, 84, 83)"
-    })
-
-    ordersName.forEach(orderName => {
-      orderName.style.color = "rgb(85, 84, 83)"
-    })
-
-  } else {
-    backgroundSection.style.backgroundImage = "url('assets/images/BandeEntete.png')";
-    backgroundFooter.style.backgroundColor = "";
-    barNav.style.backgroundColor = "";
-    backgroundFooter.style.backgroundColor = "";
-    backgroundConfi.style.backgroundColor = "";
-
-
-    allCards.forEach(card => {
-      card.style.backgroundColor = "";
-    });
-
-    categories.forEach(category => {
-      category.style.color = "";
-    });
-
-    orders.forEach(order => {
-      order.style.backgroundColor = "";
-    })
-
-
-    ordersName.forEach(orderName => {
-      orderName.style.color = ""
-    })
-
-  }
-}
-)
-
-
-
-/*
-function createCard (parent, elementType, className, textContent, src, alt, url){
-let element = document.createElement(elementType);
-
-if (className){
-element.classList.add(className)
-}
-
-if (textContent){
-element.textContent = textContent
-}
-
-if (src){
-element.setAttribute('src', src)
-}
-
-if (alt){
-element.setAttribute('alt', alt)
-}
-
-if (url){
-element.setAttribute('url', url)
-}
-
-parent.appendChild(element)
-}
-*/
-
-
-
-// Créer un tableau avec tous les genres uniques de la collection
-const allGenres = collection.reduce((acc, card) => {
-    card.genre.forEach(genre => {
-        if (!acc.includes(genre)) {
-            acc.push(genre);
-        }
-    });
-    return acc;
-}, []);
-
-const filters = allGenres;  // Utiliser les genres uniques pour les filtres
-const filterButtons = document.getElementById('filter-buttons');
-
-for (const filter of filters) {
-    const button = document.createElement('button');
-    button.textContent = filter;
-    button.addEventListener('click', () => filterCollection(filter));
-    filterButtons.appendChild(button);
-}
-
-function filterCollection(genre) {
-  // Supprimer les cartes actuelles
-  section.innerHTML = '';
-
-  // Filtrer la collection en fonction du genre sélectionné
-  const filteredCards = collection.filter((card) => card.genre.includes(genre));
-
-  // Afficher les cartes filtrées
-  for (const elem of filteredCards) {
-      let div = document.createElement('div');
-      div.className = 'card';
-
-      let divimg = document.createElement('div');
-      divimg.className = 'card__image';
-
-      let img = document.createElement('img');
-      img.src = elem.picture;
-      img.className = 'desktop-image';
-
-      divimg.appendChild(img);
-
-      let h2 = document.createElement('h2');
-      h2.className = 'card__genre';
-      h2.textContent = elem.genre.join(', ');
-
-      let h3 = document.createElement('h3');
-      h3.className = 'card__nom';
-      h3.textContent = elem.nom;
-
-      let h4 = document.createElement('h4');
-      h4.className = 'card__prix';
-      h4.textContent = elem.prix;
-
-      let p = document.createElement('p');
-      p.className = 'card__ingredients';
-      p.textContent = elem.ingredients;
-
-      let divnote = document.createElement('div');
-      divnote.className = 'card__note';
-
-      let divnoteFirst = document.createElement('div');
-      divnoteFirst.className = 'card__note__first';
-      divnoteFirst.textContent = `Note des clients ${elem.note[0]}`;
-
-      divnote.appendChild(divnoteFirst);
-
-      let divlien = document.createElement('div');
-      divlien.className = 'card__lien';
-
-      let button = document.createElement('a');
-      button.className = 'card__order';
-      button.href = elem.commander;
-      button.textContent = 'Commander le produit';
-
-      divlien.appendChild(button);
-
-      div.appendChild(divimg);
-      div.appendChild(h2);
-      div.appendChild(h3);
-      div.appendChild(h4);
-      div.appendChild(p);
-      div.appendChild(divnote);
-      div.appendChild(divlien);
-
-      section.appendChild(div);
-  }
 }
 
 //panier//
@@ -369,31 +429,32 @@ const total_count = document.querySelector(".total_count"); // Sélectionnez la 
 let cart = [];
 let total = 0;
 
-
 // Open modal
 function openCartModal() {
+  if (cartModal) {
     cartModal.style.display = "block";
-    updateCartModalContent();
+    
+  } else {
+    console.error("L'élément cartModal n'a pas été trouvé.");
   }
-  
-  // Close modal
-  function closeCartModal() {
-    cartModal.style.display = "none";
-  }
-  
-  shoppingCartButton.addEventListener("click", openCartModal);
-  closeCartModalHandler.addEventListener("click", closeCartModal);
-  
-  for (let button of addButtons) {
-    button.addEventListener(
-      "click",
-      function () {
-        addToCart(button.closest(".card"));
-        updateCartModalContent();
-      },
-      { once: true }
-    );
-  }
+}
 
+// Close modal
+function closeCartModal() {
+  cartModal.style.display = "none";
+}
 
+shoppingCartButton.addEventListener("click", openCartModal);
+closeCartModalHandler.addEventListener("click", closeCartModal);
+
+for (let button of addButtons) {
+  button.addEventListener(
+    "click",
+    function () {
+      addToCart(button.closest(".card"));
+
+    },
+    { once: true }
+  );
+}
 
